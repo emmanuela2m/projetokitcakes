0b16738520e2c5dd6bb706599b3a3890c93d75b7
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-14 11:44:49 -0300

    Mudanças na página contatos, projeto kitcakes, 14/01/2021

--- kitcakescontato.php
+++ kitcakescontato.php
@@ -1,86 +1,65 @@
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <title>Contato</title>
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
-        <link rel="stylesheet" href="assets/css/header.css" />
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css.css" />
         <link rel="stylesheet" href="assets/css/kitcakes.css"/>
     </head>
     <body>
-        <header class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark vini">
-                <div class="container">
-
-                    <a class="navbar-brand" href="#">
-                        <img src="assets/img/logo-branco.png" width="100" height="100" class="d-inline-block align-top" alt="">
-
-                    </a>
-
-                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0 ">
-                            <ul class="navbar-nav mr-auto"> 
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"><h5>Inicio</h5> <span class="sr-only">(página atual)</span></a>
-                                </li>                                                      
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"><h5>Produtos</h5> <span class="sr-only">(página atual)</span></a>
-                                </li>                                                      
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"><h5>Contatos</h5> <span class="sr-only">(página atual)</span></a>
-                                </li>                                                      
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"> <h5><span class="reg-facebook2"></span></h5></a>
-                                </li>                                                      
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"> <h5><span class="reg-instagram"></h5></span></a>
-                                </li>                                                      
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#"> <h5><span class="reg-twitter"></h5></span></a>
-                                </li>                                                      
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-
-        </header>
-        <div class="container justify-content-center px-5 pt-5">
-            <form action="">
-                <h1 class="text-center mt-4">Contato</h1>
+        <div class="container ">
+            <h1 class="text-center pb-2">Contato</h1>
+            <div class="form w-25 h-50 mx-auto ">
                 <div class="row">
                     <div class="col">
                         <div class="form-group">
-                            <label for="exampleFormControlInput1">Nome</label>
+                            <span class="manu-user"></span>  
+                            <label for="exampleFormControlInput1">Nome completo</label>                           
                             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                         </div>
-                        <div class="row"> 
-                            <div class="col">                                
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Telefone</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                            </div>
-                            <div class="col">                                
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">E-mail</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>                             
-                            </div>
-                        </div>
-
                         <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Mensagem</label>
+                            <span class="manu-whatsapp"></span>  
+                            <label for="exampleFormControlInput1">Telefone</label>
+
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-envelop"></span>
+                            <label for="exampleFormControlInput1">E-mail</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-profile"></span>
+                            <label for="exampleFormControlInput1">CPF</label>
+                            <span class="manu-document"></span>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-barcode"></span>
+                            <label for="exampleFormControlInput1">Código do Produto</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-credit-card"></span>
+                            <label for="exampleFormControlSelect1">Forma de Pagamento</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Crédito</option>
+                                <option>Débito</option>
+                                <option>Boleto</option>
+                                <option>Outros</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-bubbles4"></span>
+                            <label for="exampleFormControlTextarea1">Observação</label>
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
-                        <button class="btn btn-block btn-danger">Enviar</button>
+
+                        <button class="btn btn-block btn-danger">Enviar Pedido</button>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     </body>
 </html>
