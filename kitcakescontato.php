a8177e9abcfcb2d13e92c818247d305649545a29
Pichau <Pichau@DESKTOP-9T26SOC>
2021-01-13 22:30:01 -0200

    Projeto kitcakes, página vonato, inicio do projeto. 13/01/2021

---
+++ kitcakescontato.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contato</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
+        <link rel="stylesheet" href="assets/css/header.css" />
+        <link rel="stylesheet" href="assets/css/kitcakes.css"/>
+    </head>
+    <body>
+        <header class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark vini">
+                <div class="container">
+
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/img/logo-branco.png" width="100" height="100" class="d-inline-block align-top" alt="">
+
+                    </a>
+
+                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0 ">
+                            <ul class="navbar-nav mr-auto"> 
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Inicio</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Produtos</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Contatos</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-facebook2"></span></h5></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-instagram"></h5></span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-twitter"></h5></span></a>
+                                </li>                                                      
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+        <div class="container justify-content-center px-5 pt-5">
+            <form action="">
+                <h1 class="text-center mt-4">Contato</h1>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Nome</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="row"> 
+                            <div class="col">                                
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Telefone</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>
+                            </div>
+                            <div class="col">                                
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">E-mail</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>                             
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Mensagem</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button class="btn btn-block btn-danger">Enviar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
