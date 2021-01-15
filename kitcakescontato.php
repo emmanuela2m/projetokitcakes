a71c2f27444e0fe8fe05557783a39a9d9cd2bb19
Pichau <Pichau@DESKTOP-9T26SOC>
2021-01-14 22:23:06 -0200

    Oliver pediu para acrescentar mais coisas no formulário. 14/01/2021 21:22

--- kitcakescontato.php
+++ kitcakescontato.php
@@ -8,11 +8,12 @@
         <link rel="stylesheet" href="assets/css/kitcakes.css"/>
     </head>
     <body>
-        <div class="container ">
+        <div class="container mt-5">
             <h1 class="text-center pb-2">Contato</h1>
-            <div class="form w-50 h-50 mx-auto ">
-                <div class="row">
-                    <div class="col-4">
+            <div class="form w-75 h-50 mx-auto ">
+                <div class="row justify-content-center">
+                    <div class="col-6">
+
                         <div class="form-group">
                             <span class="manu-user"></span>  
                             <label for="exampleFormControlInput1">Nome completo</label>                           
@@ -24,8 +25,6 @@
 
                             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                         </div>
-                    </div>
-                    <div class="col-4">
                         <div class="form-group">
                             <span class="manu-envelop"></span>
                             <label for="exampleFormControlInput1">E-mail</label>
@@ -36,25 +35,60 @@
                             <label for="exampleFormControlInput1">CPF</label>
                             <span class="manu-document"></span>
                             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                        </div>
-                    </div>
-                    <div class="col-4">
+                        </div> 
                         <div class="form-group">
                             <span class="manu-barcode"></span>
-                            <label for="exampleFormControlInput1">Código do Produto</label>
+                            <label for="exampleFormControlInput1">Código do produto</label>
                             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                         </div>
-
+                    </div>
+                    <div class="col-6">
                         <div class="form-group">
                             <span class="manu-credit-card"></span>
-                            <label for="exampleFormControlSelect1">Pagamento</label>
+                            <label for="exampleFormControlSelect1">Sabor da massa</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Chocolate</option>
+                                <option>baunilha</option> 
+                            </select>
+                        </div>       
+                        <div class="form-group">
+                            <span class="manu-credit-card"></span>
+                            <label for="exampleFormControlSelect1">Sabor do recheio</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Chocolate</option>
+                                <option>Leite ninho</option> 
+                                <option>Maracujá</option> 
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-credit-card"></span>
+                            <label for="exampleFormControlSelect1">Nossas opções de kit</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>kit 1</option>
+                                <option>Kit 2</option> 
+                                <option>Kit 3</option> 
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <span class="manu-credit-card"></span>
+                            <label for="exampleFormControlSelect1">Peso da torta</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1 kg</option>
+                                <option>2 kg</option>
+                                <option>3 kg</option>
+
+                            </select>
+                        </div> 
+                        <div class="form-group">
+                            <span class="manu-credit-card"></span>
+                            <label for="exampleFormControlSelect1">Forma de pagamento</label>
                             <select class="form-control" id="exampleFormControlSelect1">
                                 <option>Crédito</option>
                                 <option>Débito</option>
                                 <option>Boleto</option>
                                 <option>Outros</option>
                             </select>
-                        </div>       
+                        </div> 
                     </div>
                 </div>
                 <div class="form-group w-100 ">
